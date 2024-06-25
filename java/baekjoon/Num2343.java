92419d402e779ef69e538a2d42ffd805d635f82e
nnWon <nwwn4627@gmail.com>
2024-06-25 14:14:07 +0900

    백준 2343번 완료

--- java/baekjoon/Num2343.java
+++ java/baekjoon/Num2343.java
@@ -1,7 +1,9 @@
 package baekjoon;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Num2343 {
 
@@ -14,19 +16,15 @@ public class Num2343 {
         st = new StringTokenizer(br.readLine());
         int[] lectures = new int[n];
         int sum = 0;
-        int maxTime = 0;
+        int longLecture = 0;
         for (int i = 0; i < n; i++) {
             int lecture = Integer.parseInt(st.nextToken());
             lectures[i] = lecture;
             sum += lecture;
-            maxTime = Math.max(maxTime, lecture);
+            longLecture = Math.max(longLecture, lecture);
         }
 
-        int min = sum / m;
-        int max = maxTime * m;
-
-
-        System.out.println(binarySearch(min, max, m, lectures));
+        System.out.println(binarySearch(longLecture, sum, m, lectures));
     }
 
     public static int binarySearch(int min, int max, int blueRay, int[] lectures) {
@@ -37,34 +35,27 @@ public class Num2343 {
             int mid = (start + end) / 2;
             int count = method(lectures, mid);
             if (blueRay < count) {
-                System.out.println("isOver!!");
                 start = mid + 1;
             } else {
-                System.out.println("Pass!!");
                 result = mid;
                 end = mid - 1;
             }
         }
-        System.out.println("end = "+ result);
         return result;
     }
 
     public static int method(int[] lectures, int target) {
         int count = 0;
         int sum = 0;
-        System.out.println("target = " + target);
         for (int i = 0; i < lectures.length; i++) {
             sum += lectures[i];
             if (target < sum) {
                 count++;
-                System.out.println("sum = "+ (sum - lectures[i]));
                 sum = 0;
                 i--;
-                System.out.println("limit index = "+ (i+1));
             }
         }
         count++;
-        System.out.println("count!!! -> " + count);
         return count;
     }
 
